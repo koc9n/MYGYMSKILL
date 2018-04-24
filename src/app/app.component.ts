fbdac676a3a66215f61f116a8079fa16559c84de
konstantin.mironchik <kos.mironchik@gmail.com>
2018-04-24 17:59:13 +0700

    added basic navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit, OnDestroy {
+  mobileQuery: MediaQueryList;
+
+  private _mobileQueryListener: () => void;
+
+  constructor(private changeDetectorRef: ChangeDetectorRef, private media: MediaMatcher) {
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  ngOnInit(): void {
+    this.mobileQuery = this.media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => this.changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,69 @@
+import {MediaMatcher} from '@angular/cdk/layout';
 import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
+import {APP_BASE_HREF} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule, Routes} from '@angular/router';
 
 import {AppComponent} from './app.component';
+import {MaterialModule} from './material/material.module';
 import {ExerciseModule} from './exercise/exercise.module';
 import {ExerciseListComponent} from './exercise/exercise-list/exercise-list.component';
+import {HomeComponent} from './home/home.component';
+import {ContactComponent} from './contact/contact.component';
+import {AboutComponent} from './about/about.component';
+import {LoginComponent} from './login/login.component';
+import {AUTH_PROVIDERS} from './auth.service';
+import {LoggedInGuard} from './logged-in.guard';
 
+const routes: Routes = [
+  // basic routes
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact-us', component: ContactComponent},
 
+  // authentication demo
+  {path: 'login', component: LoginComponent},
+  {
+    path: 'exercises',
+    component: ExerciseListComponent,
+    // canActivate: [LoggedInGuard]
+  },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes), // <-- routes
+    MaterialModule,
     ExerciseModule,
   ],
-  providers: [],
+  providers: [
+    // uncomment this for "hash-bang" routing
+    // { provide: LocationStrategy, useClass: HashLocationStrategy }
+    {provide: APP_BASE_HREF, useValue: '/'},
+    AUTH_PROVIDERS,
+    LoggedInGuard,
+    MediaMatcher,
+  ],
   bootstrap: [AppComponent],
-  schemas: [NO_ERRORS_SCHEMA]
+  schemas: [NO_ERRORS_SCHEMA],
 })
 export class AppModule {
 }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
